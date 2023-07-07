8515d9d3161014ee11f12127020ead8eaaf54c48
l <61829586+Whesley696@users.noreply.github.com>
2023-07-07 14:10:42 +0000

    deploy

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TesteHome(TestCase):
+    def setUp(self):
+         self.response = self.client.get('/')
+    def test_home(self):
+        self.assertEqual(self.response.status_code,200) 
+    def test_template_used(self): 
+        self.assertTemplateUsed(self.response,'index.html')
