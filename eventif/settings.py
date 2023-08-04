64e3f2ece9d2a07c9b89a170b5290c6ba5d8ab7f
l <61829586+Whesley696@users.noreply.github.com>
2023-08-04 12:57:55 +0000

    brigado

--- eventif/settings.py
+++ eventif/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core"
+    "core",
+    "subscriptions",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = "eventif.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS":  ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- eventif/urls.py
+++ eventif/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
+from subscriptions.views import subscribe
 urlpatterns = [
   path('', home),
-  path('admin/', admin.site.urls)
+  path('admin/', admin.site.urls),
+  path('inscricao/', subscribe),
+
 ]
 
 
