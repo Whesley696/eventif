469d3fa1d663335ab97b585140e2aa3bd3a76223
l <61829586+Whesley696@users.noreply.github.com>
2023-08-18 10:26:44 -0300

    Update views.py

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,14 +1,25 @@
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from subscriptions.forms import SubscriptionForm
 from django.core import mail
 from django.template.loader import render_to_string
+from django.http import HttpResponse  # Import necessário
+
+
 def subscribe(request):
     if request.method == 'POST':
         form = SubscriptionForm(request.POST)
-        form.full_clean()
-        body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-        mail.send_mail('Confirmação de inscrição','Message','whesleysouza21@gmail.com',['patrick.souza@aluno.riogrande.ifrs.edu.br', form.cleaned_data['email']])
-        return HttpResponseRedirect('/inscricao/')
-    context={'form':SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+        if form.is_valid():
+            body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+            mail.send_mail('Confirmação de inscrição', body, 'whesleysouza21@gmail.com', ['patricksouza@gmail.com', form.cleaned_data['email']])
+            messages.success(request, 'Inscrição realizada com sucesso')
+            return HttpResponseRedirect('/inscricao/')  # Movido para dentro do bloco if
+        else:
+            return render(request, 'subscriptions/subscription_form.html', {'form':form})
+
+    else: 
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
+
+
