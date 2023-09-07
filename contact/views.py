4fc088722b7d7527bacfdc0ae83911252c4da30d
l <61829586+Whesley696@users.noreply.github.com>
2023-09-06 23:57:46 -0300

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -11,27 +11,20 @@ def contact_view(request):
         return create(request);
     else:
         return new(request);
-
 def create(request):
     form = ContactForm(request.POST)
-
     if not form.is_valid():
         return render(request, 'contact/contact_form.html', {'form': form})
-
     nome = form.cleaned_data['nome'];
     telefone = form.cleaned_data['telefone']
     email = form.cleaned_data['email'];
     mensagem = form.cleaned_data['mensagem']
-
     assunto = 'contato@eventif.com.br'  ;
     remetente = email  ;
     lista_destinatarios = ['contato@eventif.com.br', email]  
-
     conteudo_email = render_to_string('contact/contact_email.txt', {'name': nome,'phone': telefone,'email': email, 'message': mensagem
     })
-
     send_mail(assunto, conteudo_email, remetente, lista_destinatarios)
-
     messages.success(request, 'Mensagem enviada com sucesso');
     return HttpResponseRedirect('/contact/')
 
