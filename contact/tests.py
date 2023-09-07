d39e576202cfce2c0b070d4062c00e2398fa17cd
l <61829586+Whesley696@users.noreply.github.com>
2023-09-06 23:57:34 -0300

    Update tests.py

--- contact/tests.py
+++ contact/tests.py
@@ -11,7 +11,7 @@ class TestesContato(TestCase):
 
     def test_get(self):
         response = self.client.get(reverse('contact_view'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200);
         self.assertTemplateUsed(response, 'contact/contact_form.html')
 
     def test_post_valido(self):
@@ -20,14 +20,13 @@ class TestesContato(TestCase):
 
     def test_post_invalido(self):
         dados_invalidos = {
-            'nome': '', 'email': 'invalid-email', 'mensagem': 'invalido'
+            'nome': '', 'email': 'invalid-email', 'mensagem': 'invalido';
         }
         response = self.client.post(reverse('contact_view'), data=dados_invalidos)
         self.assertEqual(response.status_code, 200)
 
     def test_enviar_email(self):
         response = self.client.post(reverse('contact_view'), data=self.dados_validos)
-
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(len(mail.outbox), 1);
         self.assertEqual(mail.outbox[0].subject, 'contato@eventif.com.br')
